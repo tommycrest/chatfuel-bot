8b5a04251cf2fb66d8ad6092625524d37c1e084f
Tommy <tommy.crestanello@digitaslbi.com>
2016-12-15 17:43:57 +0100

    commit del link alla ricerca di google

--- routes/messages.js
+++ routes/messages.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
@@ -11,12 +13,50 @@ router.post('/', function(req, res, next) {
     res.end();
 });
 
+function escapeSpecialChars(str) { 
+    return str.replace(/\\/g, "\\\\")
+        .replace(/\n/g, "\\n")
+        .replace(/\r/g, "\\r")
+        .replace(/\t/g, "\\t")
+        .replace(/\f/g, "\\f")
+        .replace(/"/g,"\\\"")
+        .replace(/'/g,"\\\'")
+        .replace(/\&/g, "\\&"); 
+} 
+
 // metodo per effettuare la chiamata a google ed 
 // ottenere il json per la costruzione della risposta
 // http://www.google.it/search?q=post+su+google+con+express
 function requestToGoogle(queryStr){
-    var retVal = router.post('http://www.google.it/search?q='+queryStr);
-    console.log(retVal);
+
+  console.log(queryStr);
+  var myJSON = JSON.stringify(queryStr);
+  var myEscapedJSONString = escapeSpecialChars(myJSON);
+    
+   console.log(myEscapedJSONString.substr(2,myEscapedJSONString.length-4));
+    
+   var options = {
+        host: 'www.google.it',
+        path: '/search?q='+myEscapedJSONString.substr(2,myEscapedJSONString.length-4).replace(/\ /g, "\+")
+   };
+    
+    
+   callback = function(response) {
+    var str = '';
+
+    //another chunk of data has been recieved, so append it to 
+    response.on('data', function (chunk) {
+        str += chunk;
+    });
+
+    //the whole response has been recieved, so we just print it out here
+    response.on('end', function () {
+        console.log(str);
+        response.send(str);
+    });
+ }
+
+   http.request(options, callback).end();
 };
 
 module.exports = router;
