a32b57245c3f562163bd9ae95222f0adc9bca422
Tommy <tommy.crestanello@digitaslbi.com>
2016-12-15 15:14:42 +0100

    post su google

--- routes/messages.js
+++ routes/messages.js
@@ -4,7 +4,17 @@ var router = express.Router();
 /* GET users listing. */
 router.post('/', function(req, res, next) {
     console.log(req.body);
+    
+    res.send();
     res.end();
 });
+
+// metodo per effettuare la chiamata a google ed 
+// ottenere il json per la costruzione della risposta
+// http://www.google.it/search?q=post+su+google+con+express
+function requestToGoogle(queryStr){
+    var retVal = router.post('http://www.google.it/search?q='+queryStr);
+    console.log(retVal);
+};
 
 module.exports = router;
