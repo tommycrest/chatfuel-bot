196d092842ac2a8308a4700d841ec4e36d86791a
Tommy <tommy.crestanello@digitaslbi.com>
2016-12-22 15:45:14 +0100

    wiki request

--- routes/product.js
+++ routes/product.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('product list from elastic search');
+  res.send('{\"messages\": [{\"attachment\": {\"type\": \"template\",\"payload\": {\"template_type\": \"button\",\"text\": \"Hello!\",\"buttons\": [{\"type\": \"show_block\",\"block_name\": \"some block name\",\"title\": \"Show the block!\"},{\"type\": \"web_url\",\"url\": \https://petersapparel.parseapp.com/buy_item?item_id=100\",\"title\": \"Buy Item\"}]}}}]}');
 });
 
 module.exports = router;
